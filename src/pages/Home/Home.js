d4db4a578d2b9c44d8b6414379d4d17ff80a4a5f
Akil Chowdhury <akiluddinchy@gmail.com>
2022-11-07 23:33:07 +0600

    add banner

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -2,8 +2,18 @@ import React from 'react';
 
 const Home = () => {
     return (
-        <div>
-            <h1>Home</h1>
+        <div className="p-8">
+            <div className="flex justify-evenly text-4xl font-bold px-44 py-8">
+                <h1>MODERN</h1>
+                <h1>.</h1>
+                <h1>ERGONOMIC</h1>
+                <h1>.</h1>
+                <h1>DESIGN</h1>
+            </div>
+            <div className="flex justify-center">
+                <img className="w-full h-screen" src="https://images.unsplash.com/photo-1431576901776-e539bd916ba2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1171&q=80" alt="banner-img" />
+            </div>
+            
         </div>
     );
 };
