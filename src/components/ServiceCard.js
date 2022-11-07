a4c4fb64c13aa77d6c4127441cf2d32582e60269
Akil Chowdhury <akiluddinchy@gmail.com>
2022-11-08 01:18:41 +0600

    add service cards in home

--- src/components/ServiceCard.js
+++ src/components/ServiceCard.js
@@ -1,11 +1,26 @@
-import React from 'react';
+import React from "react";
 
-const ServiceCard = () => {
-    return (
-        <div>
-            
+const ServiceCard = ({ service }) => {
+  const { _id, name, price, description, picture } = service;
+
+  const trimmedDescription = description.slice(0, 100);
+
+  console.log(name);
+  return (
+    <div className="card bg-base-100 shadow-xl">
+      <figure className="px-10 pt-10">
+        <img src={picture} alt={name} className="w-full h-64" />
+      </figure>
+      <div className="card-body items-center text-center">
+        <h2 className="card-title">{name}</h2>
+        <p>Price: {price} lacs</p>
+        <p>{trimmedDescription}...</p>
+        <div className="card-actions">
+          <button className="btn btn-primary">View Details</button>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default ServiceCard;
